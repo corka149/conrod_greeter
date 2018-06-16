5dbd7bb7945296c5612802c50289287a9d71c6d7
corka149 <corka149@mailbox.org>
2018-06-16 09:49:00 +0200

    Moved UiCore to separate mod

---
+++ src/core/mod.rs
@@ -0,0 +1,32 @@
+use conrod::backend::glium::glium::{self, Surface};
+use conrod::{self, widget, Colorable, Positionable, Widget};
+
+pub struct UiCore {
+    pub events_loop: glium::glutin::EventsLoop,
+    pub display: glium::Display,
+    pub ui: conrod::Ui
+}
+
+impl UiCore {
+
+    pub fn new(title: String, init_width: u32, init_height: u32) -> UiCore {
+
+        // which will handle interaction with the UI
+        let mut events_loop = glium::glutin::EventsLoop::new();
+        let window = glium::glutin::WindowBuilder::new()
+            .with_title(title)
+            .with_dimensions(init_width, init_height);
+        let context = glium::glutin::ContextBuilder::new()
+            .with_vsync(true)
+            .with_multisampling(4);
+        // The display is the home for the UI, and is an OpenGL context provided by glium.
+        let display = glium::Display::new(window, context, &events_loop).unwrap();
+        let mut ui = conrod::UiBuilder::new([init_width as f64, init_height as f64]).build();
+
+        UiCore {
+            events_loop,
+            display,
+            ui
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate conrod;
 
+mod core;
 mod util;
 
 use conrod::backend::glium::glium::{self, Surface};
@@ -8,27 +9,14 @@ use conrod::{widget, Colorable, Positionable, Widget};
 use util::EventLoop;
 
 pub fn bootstrap() {
-  const WIDTH: u32 = 400;
-  const HEIGHT: u32 = 200;
-
-  // which will handle interaction with the UI
-  let mut events_loop = glium::glutin::EventsLoop::new();
-  let window = glium::glutin::WindowBuilder::new()
-    .with_title("Hello Conrod")
-    .with_dimensions(WIDTH, HEIGHT);
-  let context = glium::glutin::ContextBuilder::new()
-    .with_vsync(true)
-    .with_multisampling(4);
-  // The display is the home for the UI, and is an OpenGL context provided by glium.
-  let display = glium::Display::new(window, context, &events_loop).unwrap();
-  let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+  let mut ui_core = core::UiCore::new(String::from("Conrod Greeter"), 400, 300);
 
   // a macro provided by conrod to create widget ids. Conrod's primary data structure contains all
   // the widgets, and uses their ids to keep track of them. The widget_ids! macro just provides a
   // very quick and easy way of defining widget names (in this case just text) and giving them ids.
   widget_ids!(struct Ids { text });
   // Ids::new creates the the widget structure.
-  let ids = Ids::new(ui.widget_id_generator());
+  let ids = Ids::new(ui_core.ui.widget_id_generator());
 
   // Conrod can use graphics. It stores these in a map. The system needs the map,
   // even though it doesn't contain anything at this time, so create it:
@@ -39,16 +27,16 @@ pub fn bootstrap() {
     env!("CARGO_MANIFEST_DIR"),
     "/assets/fonts/NotoSans/NotoSans-Regular.ttf"
   );
-  ui.fonts.insert_from_file(FONT_PATH).unwrap();
+  ui_core.ui.fonts.insert_from_file(FONT_PATH).unwrap();
 
   // Finally, Conrod needs to render its UI. It uses a renderer to do this, so create one:
-  let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+  let mut renderer = conrod::backend::glium::Renderer::new(&ui_core.display).unwrap();
 
   // As an Immediate Mode GUI, Conrod sits in the main loop of the program, drawing the UI every
   // time round the loop. Here's the main loop:
   'main: loop {
     let mut event_loop = EventLoop::new();
-    for event in event_loop.next(&mut events_loop) {
+    for event in event_loop.next(&mut ui_core.events_loop) {
       match event.clone() {
         glium::glutin::Event::WindowEvent { event, .. } => match event {
           glium::glutin::WindowEvent::Closed
@@ -68,14 +56,14 @@ pub fn bootstrap() {
       // add event handling for Conrod. Putting the following at the beginning of the event
       // for loop will take care of UI events
       if let Some(event) =
-      conrod::backend::winit::convert_event(event, &display)
+      conrod::backend::winit::convert_event(event, &ui_core.display)
         {
           // ui.handle_event() is the business end of Conrod - it takes events off the queue,
           // works out which widget they apply to and looks after dispatch, etc.
-          ui.handle_event(event);
+          ui_core.ui.handle_event(event);
         }
 
-      let ui = &mut ui.set_widgets();
+      let ui = &mut ui_core.ui.set_widgets();
       // "Hello World!" in the middle of the screen.
       widget::Text::new("Hello World!")
         .middle_of(ui.window)
@@ -85,14 +73,14 @@ pub fn bootstrap() {
     }
 
     // Render the `Ui` and then display it on the screen.
-    if let Some(primitives) = ui.draw_if_changed() {
-      renderer.fill(&display, primitives, &image_map);
+    if let Some(primitives) = ui_core.ui.draw_if_changed() {
+      renderer.fill(&ui_core.display, primitives, &image_map);
 
       // starts drawing on the backbuffer
-      let mut target = display.draw();
+      let mut target = ui_core.display.draw();
       target.clear_color(0.0, 1.0, 0.0, 1.0);
       renderer
-        .draw(&display, &mut target, &image_map)
+        .draw(&ui_core.display, &mut target, &image_map)
         .unwrap();
       target.finish().unwrap();
     }
